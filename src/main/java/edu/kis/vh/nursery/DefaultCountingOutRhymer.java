94fab744ef9f874400ab0425bf75e65bb8009068
Vatnax <krzwojtal@gmail.com>
2025-03-29 20:22:15 +0100

    Zmiana widoczności atrybutów na możliwie najmniejszą

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,14 +2,14 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int MAX_SIZE  = 12;
-    public static final int STARTING_VALUE = -1;
-    public static final int FAILURE_CODE = -1;
-    public static final int EMPTY_INDICATOR  = -1;
-    public static final int FULL_INDICATOR  = 11;
+    private static final int MAX_SIZE  = 12;
+    private static final int STARTING_VALUE = -1;
+    private static final int FAILURE_CODE = -1;
+    private static final int EMPTY_INDICATOR  = -1;
+    private static final int FULL_INDICATOR  = 11;
     private final int[] numbers = new int[MAX_SIZE ];
 
-    public int total = STARTING_VALUE;
+    private int total = STARTING_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
